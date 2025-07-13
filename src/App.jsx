7fb12117408327a1dc0d0d508afcf8a48e9e406f
Muhammad hassan <hassanhanjra900@gmail.com>
2025-07-13 12:16:45 -0400

    update:

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-function App() {
-  return (
-    <main className="flex items-center justify-center h-screen bg-black text-white">
-      <h1 className="text-4xl font-italic">H4ssan.dev</h1>
-    </main>
-  );
+function App(){
+  return <>
+  <p>h4ssan.dev</p>
+  </>
 }
 
+
 export default App;
+
+// import { useState } from 'react';
+
+// function TodoApp() {
+//   const [todos, setTodos] = useState([]);
+//   const [input, setInput] = useState('');
+
+//   const addTodo = () => {
+//     if (!input) return;
+//     setTodos([...todos, input]);
+//     setInput('');
+//   };
+
+//   return (
+//     <>
+//       <input value={input} onChange={e => setInput(e.target.value)} />
+//       <button onClick={addTodo}>Add</button>
+//       <ul>
+//         {todos.map((t, i) => <li key={i}>{t}</li>)}
+//       </ul>
+//     </>
+//   );
+// }
+
+
+// export default TodoApp;
